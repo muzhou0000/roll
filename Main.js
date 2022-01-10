f86caaa52fa5fe8421e2c5465997c048c78c43a8
muzhou0000 <bono60131@gmail.com>
2022-01-10 21:58:15 +0800

    GO

--- Main.js
+++ Main.js
@@ -3,13 +3,44 @@ var View;
 (function (View) {
     class view {
         constructor(controller, model) {
-            this._app = new PIXI.Application(905, 1065, { backgroundColor: 0xD1E292 });
+            this._app = new PIXI.Application(300, 300, { backgroundColor: 0xD1E292 });
             document.body.appendChild(this._app.view);
             this._model = model;
             this._controller = controller;
             this.init();
         }
-        init() { }
+        init() {
+            let text = new PIXI.Text('大吉大利', { fill: 0xffffff });
+            this._app.stage.addChild(text);
+            text.anchor.set(0.5, 0.5);
+            text.x = this._app.screen.width / 2;
+            text.y = this._app.screen.height / 2;
+            let btn = new PIXI.Graphics();
+            btn.beginFill(0xffffff);
+            btn.drawRoundedRect(0, 0, 90, 20, 5);
+            btn.endFill();
+            btn.x = text.x - 45;
+            btn.y = text.y + 30;
+            btn.buttonMode = true;
+            btn.interactive = true;
+            this._app.stage.addChild(btn);
+            btn.on('click', () => {
+                text.text = this.click();
+            });
+        }
+        click() {
+            let str = [
+                '大吉',
+                '中吉',
+                '小吉',
+                '末吉',
+                '小凶',
+                '凶',
+                '大凶',
+            ];
+            let roll = str[Math.floor(Math.random() * str.length)];
+            return roll;
+        }
     }
     View.view = view;
 })(View || (View = {}));
--- view.ts
+++ view.ts
@@ -7,7 +7,7 @@ namespace View {
 
         constructor(controller: Controller.controller, model: Model.model) {
 
-            this._app = new PIXI.Application(905, 1065, { backgroundColor: 0xD1E292 });
+            this._app = new PIXI.Application(300, 300, { backgroundColor: 0xD1E292 });
             document.body.appendChild(this._app.view);
 
             this._model = model;
@@ -16,7 +16,42 @@ namespace View {
         }
 
 
-        private init(): void {}
+        private init(): void {
+
+            let text:PIXI.Text=new PIXI.Text('大吉大利',{fill:0xffffff});
+            this._app.stage.addChild(text);
+            text.anchor.set(0.5,0.5);
+            text.x=this._app.screen.width/2;
+            text.y=this._app.screen.height/2;
+            
+            let btn:PIXI.Graphics=new PIXI.Graphics();
+            btn.beginFill(0xffffff);
+            btn.drawRoundedRect(0,0,90,20,5);
+            btn.endFill();
+            btn.x=text.x-45;
+            btn.y=text.y+30;
+            btn.buttonMode=true;
+            btn.interactive=true;
+            this._app.stage.addChild(btn);
+            btn.on('click',()=>{
+                text.text=this.click();
+            })
+        }
+        private click():string{
+            let str:string[]=[
+                '大吉',
+                '中吉',
+                '小吉',
+                '末吉',
+                '小凶',
+                '凶',
+                '大凶',
+            ]
+            
+            let roll:string=str[Math.floor(Math.random() *str.length)];
+
+            return roll;
+        }
 
    }
 }
